772965ff424df94e9eca3ab2a754d7c1e1aee388
SNikx0n <fenix0596@mail.ru>
2021-04-30 18:04:45 +0300

    Page Object #1

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,28 @@
 # pytest -v --tb=line --language=en test_main_page.py
 
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     browser.get(link)
+#     login_link = browser.find_element_by_css_selector("#login_link")
+#     login_link.click()
+
+# def test_add_to_cart(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/category/books_2/"
+#     page = ProductPage(url="", browser)   # инициализируем объект Page Object
+#     page.open()                           # открываем страницу в браузере
+#     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+#     page.add_product_to_cart()            # жмем кнопку добавить в корзину
+#     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+
+
+
